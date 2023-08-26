b5b24a98f100570502d5016321db09e7d28639c2
ixelizm <felekburak8@gmail.com>
2023-08-27 01:24:17 +0300

    added!

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask
 from flask_restful import Resource, Api
-
+from config import API_ID
 app = Flask(__name__)
 api = Api(app)
 
 class Greeting(Resource):
     def get(self):
-        return "XUserbot Aktif Ve Çalışıyor!"
+        return "XUserbot Aktif Ve Çalışıyor!" + str(API_ID)
 
 api.add_resource(Greeting, '/')
 
