280c96042b63c423ac414a7c6d818e19c9d24521
ixelizm <felekburak8@gmail.com>
2023-08-30 01:17:44 +0300

    added!

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -3,9 +3,11 @@ from pymongo import MongoClient
 class Database:
   def __init__(x):
     x.client = MongoClient('mongodb://141.98.115.181:27017/')
- 
   def insert_data(x, col, vars):
     result = x.DATABASE[col].insert_one(vars)
     return result.inserted_id
   def initialize_db(x, user_id):
     x.DATABASE = x.client[str(user_id)]
+  def get_data(x, col, query):
+    result = x.DATABASE[col].find_one(query)
+    return result
--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -12,9 +12,12 @@ async def al(_,m):
 
 @Client.on_message(filters.command("test", ".") &filters.me)
 async def addata(_,m):
-  result = Db.insert_data(m.from_user.id, "testtablo", {"bot":"xuserbot"})
+  result = Db.insert_data("testtablo", {"bot":"xuserbot"})
   await m.edit(result)
-
+@Client.on_message(filters.command("get", ".") &filters.me)
+async def getata(_,m):
+  result = Db.get_data("testtablo", {"bot":"xuserbot"})
+  await m.edit(result)
 CmdHelp('alive').add_command(
     'alive', None, "Botun çalışıp çalışmadığını kontrol edebilirsiniz."
 ).add()
