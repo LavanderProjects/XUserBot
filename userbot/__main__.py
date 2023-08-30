9c38bff720d8a5c3ef553f5864b026e26cec78d8
ixelizm <felekburak8@gmail.com>
2023-08-30 11:38:13 +0300

    added!

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,6 +5,6 @@ from userbot import app, Db
 if __name__ == "__main__":
   app.start()
   me = app.get_me()
-  Db.connect(me.id)
+  Db.connect("XBase.db")
   app.send_message("me", "`Database is initialized!`")
   idle()
