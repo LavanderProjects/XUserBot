61df232aebe9f47040133173b5a9ee00b495206a
ixelizm <felekburak8@gmail.com>
2023-08-31 11:59:12 +0300

    added!

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,10 +1,15 @@
-from pyrogram import Client, idle
-
+from pyrogram import Client, idle, enums
+import json
 from userbot import app, Db
-
+from config import BOTLOG
 if __name__ == "__main__":
   app.start()
   me = app.get_me()
-  Db.connect("XBase.db")
-  app.send_message("me", "`Database is initialized!`")
+  Db.connect("x.sql")
+  for message in app.search_messages(chat, filter=enums.MessagesFilter.DOCUMENT):
+    app.send_message("me", message.document.file_name)
+#    docs = message.download()
+#  with open(docs) as f:
+#    data = json.load(f) 
+#  app.send_message("me", "`Database is initialized!`")
   idle()
