5eb20c802333685ba26c565c3c6714546a6befb4
ixelizm <felekburak8@gmail.com>
2023-08-31 12:17:25 +0300

    added!

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
   me = app.get_me()
   Db.connect("x.sql")
   for message in app.search_messages(int(BOTLOG), filter=enums.MessagesFilter.DOCUMENT):
-    app.send_message("me", message.document.file_name)
-#    docs = message.download()
-#  with open(docs) as f:
-#    data = json.load(f) 
-#  app.send_message("me", "`Database is initialized!`")
+    if message.document.file_name == "x.conf":
+      docs = message.download()
+      with open(docs) as f:
+        data = json.load(f)
+      Db.json2db(data)
+  app.send_message("me", "`Database is initialized!`")
   idle()
