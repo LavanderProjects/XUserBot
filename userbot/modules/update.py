a7252ca059136e89df4e695856144b79fb04805b
ixelizm <felekburak8@gmail.com>
2023-09-02 01:10:45 +0300

    added!

--- userbot/modules/update.py
+++ userbot/modules/update.py
@@ -19,9 +19,4 @@ async def update_command(_, m):
       with open("userbot/modules/" + file + ".py", "w") as f:
         f.write(data)
     await asyncio.sleep(1)
-    url = "https://api.render.com/v1/services?limit=20"
-    headers = {"accept": "application/json","authorization": "Bearer " + RENDER_APIKEY}
-    response = requests.get(url, headers=headers)
-    servis = response.json()[0]["service"]
-    url = f"https://api.render.com/v1/services/{servis['id']}/restart"
-    response = requests.post(url, headers=headers)
+    os.execl(sys.executable, sys.executable, "-m", "userbot")
