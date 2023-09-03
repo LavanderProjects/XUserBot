48dfc1a2aee11f64d089af6e550bbe7ee3c9bd85
ixelizm <felekburak8@gmail.com>
2023-09-03 15:13:17 +0300

    added!

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -15,7 +15,7 @@ async def al(_,m):
     await m.edit("db is writed!")
   else:
     await m.reply(str(Db.data))
-    await _.send_message(m.chat.id, str(Db.get_record_by_key(m.command[1], m.command[2])))
+    await _.send_message(m.chat.id, str(Db.get_record_by_key(m.command[1], m.command[2], int(m.command[3]))))
 CmdHelp('alive').add_command(
     'alive', None, "Botun çalışıp çalışmadığını kontrol edebilirsiniz."
 ).add()
