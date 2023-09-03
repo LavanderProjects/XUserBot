f2cc3e496aed66860fe21900a25768e94d5a3f71
ixelizm <felekburak8@gmail.com>
2023-09-03 15:30:50 +0300

    added!

--- userbot/modules/snips.py
+++ userbot/modules/snips.py
@@ -5,11 +5,11 @@ from userbot import Db
 
 @Client.on_message(filters.regex(r"\$\w+") &filters.me)
 async def snipwh(_,m):
-  record = db.get_record_by_key("snips", "snip", m.text.replace("$",""))
+  record = Db.get_record_by_key("snips", "snip", m.text.replace("$",""))
   await m.delete()
   await _.copy_message(m.chat.id, "me", record["message_id"])
 
-@Client.on_message(filters.command("snip", "") &filters.me)
+@Client.on_message(filters.command("snip", ".") &filters.me)
 async def snipadd(_,m):
   if m.reply_to_message:
     copied_message = await m.copy("me")
