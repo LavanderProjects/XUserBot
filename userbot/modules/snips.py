24a153de353ddddec414a0f374eda7094bfdc991
ixelizm <felekburak8@gmail.com>
2023-09-03 15:36:40 +0300

    added!

--- userbot/modules/snips.py
+++ userbot/modules/snips.py
@@ -9,6 +9,7 @@ async def snipwh(_,m):
   await m.delete()
   await _.copy_message(m.chat.id, "me", record["message_id"])
 
+
 @Client.on_message(filters.command("snip", ".") &filters.me)
 async def snipadd(_,m):
   if m.reply_to_message:
@@ -16,3 +17,16 @@ async def snipadd(_,m):
     data = {"snip": m.command[1], "message_id": copied_message.id}
     Db.insert_record("snips", data)
     await m.edit("`snip is saved!`")
+
+@Client.on_message(filters.command("snips", ".") &filters.me)
+async def snips(_,m):
+  allsnips = Db.data["snips"]
+  text = "**All Snips:**\n"
+  for snip in allsnips:
+    text += " - " + snip["snip"] + "\n"
+  await m.edit(text)
+@Client.on_message(filters.command("rmsnip", ".") &filters.me)
+async def sniprem(_,m):
+  snip = m.command[1]
+  Db.delete_record("snips", "snip", snip)
+  await m.edit("`Snip is removed!`")
